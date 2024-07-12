73ba5282f4211100656447276bd6d3f4fefceabe
Raymond Lin <86033049+ZaRamen@users.noreply.github.com>
2024-07-12 01:46:25 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -59,7 +59,7 @@ const SpotifyProfile: React.FC = () => {
   const initialRender = useRef(true);
   const [songInfo, setSongInfo] = useState<SongInfo | null>(null);
 
-  useEffect(() => {
+  useEffect( async () => {
      try {
           const result = await fetch("https://api.spotify.com/v1/me", {
             method: "GET", headers: { Authorization: `Bearer ${accessToken}` }
